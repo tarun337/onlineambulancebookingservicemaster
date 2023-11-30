f63bc9c8b2116b7ae400626c6d28783350c34c35
tarunmeka <108917050+tarun337@users.noreply.github.com>
2023-11-30 16:10:36 +0530

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,8 +1,8 @@
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_HOST','mysqlser1.mysql.database.azure.com');
+define('DB_USER','tarun');
+define('DB_PASS','Sridevi@31');
 define('DB_NAME','carrental');
 // Establish database connection.
 try
